e4ae3a17516b9a2999fa91d8653284c0f1693c9c
Ilia Riabsev <46lidiay@gmail.com>
2025-12-09 22:35:17 +0200

    Added Blue Bus Line

--- src/domain/services/VehicleTypeMapper.ts
+++ src/domain/services/VehicleTypeMapper.ts
@@ -3,7 +3,7 @@ import type { VehicleType } from '../types'
 export class VehicleTypeMapper {
   static fromRouteType(routeType: number): VehicleType | 'other' {
     if ([0, 900].includes(routeType)) return 'tram'
-    if ([3, 700].includes(routeType)) return 'bus'
+    if ([3, 700, 11].includes(routeType)) return 'bus'
     return 'other'
   }
 }
